7b4760ddfba176fabb1cea515fe8dd7de86f5ec6
Mauricio-TW <mauriciothumwelter@gmail.com>
2024-02-26 11:15:00 -0300

    indo 2

---
+++ cadstro.php
@@ -0,0 +1,18 @@
+<?php
+        include("conexao.php");
+        $nome = $_POST["nome"];
+        $sobrenome = $_POST["sobrenome"];
+        $email = $_POST["email"];
+        $senha = $_POST["senha"];
+        
+        $sql = mysqli_query($banco,"insert into cadastro values(null,'$nome','$sobrenome','$email','$telefone','$curso','$senha','');");
+        if ($sql) { 
+            echo "Contato cadastrado com sucesso";
+            echo"<META http-equiv='refresh' content='5,URL=index.php'>";
+        } else {
+            echo "Nâo foi possível cadastrar<br>Causa:".mysqli_error($banco) ;
+            
+        }
+        mysqli_close($banco);
+
+    ?>
