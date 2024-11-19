2c5dc15c82c5150dc397c2158580d391c801afe9
Mauricio-TW <115851331+Mauricio-TW@users.noreply.github.com>
2024-11-19 18:00:08 -0300

    Exclusão de coisas desnecessarias

--- back/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    $banco = mysqli_connect("localhost","root","","academia_boxe",);
-    if(!$banco){
-         //echo "Não foi possivel conectar com o BD.<br>Causa:".mysqli_connect_error();
-    } else {
-         //echo "Conexão com o BD ocorreu normalmente.<br>";
-    }
-?>
